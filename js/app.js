7338d5e75de8e2a0957c3f28b739620d633db926
marcinbrylka <marcin_brylka@wp.pl>
2018-06-20 10:55:54 +0200

    Added flood fill algorithm

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ import '../scss/main.scss';
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const saperPicture = document.querySelector(".saperPicture").style.backgroundImage = "url(images/face_unpressed.png)";
+    const saperPicture = document.querySelector(".saperPicture");
+    saperPicture.style.backgroundImage = "url(images/face_unpressed.png)";
     const main = document.querySelector(".mainContainer");
     const board = document.querySelector(".boardContainer");
     const info = document.querySelector(".informationContainer");
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let cells = [];
     let cellsNumbers = [];
     let numberOfCells;
+    let floodFillCounter = 0;
 
     class Minesweeper {
         constructor(boardColumns, boardRows, numberOfMines) {
@@ -54,11 +56,13 @@ document.addEventListener("DOMContentLoaded", function () {
                     //     }
                     // });
 
-                    // cells[i][j].addEventListener("mouseup", () => {
-                    //     if (cellsNumbers[i][j] === 0) {
-                    //         this.floodFIll(i, j);
-                    //     }
-                    // });
+                    cells[i][j].addEventListener("mouseup", (e) => {
+                        if (e.button === 0) {
+                            if (cellsNumbers[i][j] === 0) {
+                                this.reveal(i, j);
+                            }
+                        }
+                    });
 
                     cells[i][j].addEventListener("mouseup", function (e) {
                         if (e.button === 0) {
@@ -68,10 +72,13 @@ document.addEventListener("DOMContentLoaded", function () {
                             if (this.innerText === "?") {
                                 return false;
                             }
-                            if (cellsNumbers[i][j] === 0) {
-                                // this.floodFill(i, j);
-                                this.className = "known";
-                            }
+                            // if (cellsNumbers[i][j] === 0) {
+                            //     // reveal(i, j);
+                            //     console.log(floodFillCounter);
+                            //     // this.floodFill(i, j);
+                            //     this.className = "known";
+                            //     // minesweeper.floodFill(i, j);
+                            // }
                             if (cellsNumbers[i][j] >= 1 && cellsNumbers[i][j] <= 8) {
                                 this.className = "known";
                                 for (let k = 1; k < 9; k++) {
@@ -118,6 +125,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
 
+        clearBoard() {
+            const cell = document.querySelectorAll(".boardContainer > div");
+            for (let i = 0; i < numberOfCells; i++) {
+                cell[i].remove();
+            }
+            cells = [];
+            cellsNumbers = [];
+        }
 
         createCellsNumbers() {
             for (let i = 0; i < this.boardRows; i++) {
@@ -176,22 +191,73 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         };
 
-        floodFIll(x, y) {
-            if (x < 0 && x >= this.boardColumns && y < 0 && y >= this.boardRows) {
-                return false;
+        // floodFIll(x, y) {
+        //     if (x < 0 && x >= this.boardColumns && y < 0 && y >= this.boardRows) {
+        //         return false;
+        //     }
+        //     if (cellsNumbers[x][y] !== 0) {
+        //         return false;
+        //     }
+        //     cells[x][y].className = "known";
+        //     floodFill(x, y - 1);
+        //     floodFill(x + 1, y - 1);
+        //     floodFill(x + 1, y);
+        //     floodFill(x + 1, y + 1);
+        //     floodFill(x, y + 1);
+        //     floodFill(x - 1, y + 1);
+        //     floodFill(x - 1, y);
+        //     floodFill(x - 1, y - 1);
+        // }
+
+        floodFill(x, y) {
+            for (let i = -1; i < 2; i++) {
+                for (let j = -1; j < 2; j++) {
+                    if (x + i >= 0 && x + i < this.boardColumns && y + j >= 0 && y + j < this.boardRows) {
+                        if (cellsNumbers[x + i][y + j] !== -1 && cells[x + i][y + j].className !== "known") {
+                            this.reveal(x + i, y + j);
+
+                        }
+                    }
+                }
             }
-            if (cellsNumbers[x][y] !== 0) {
-                return false;
+
+
+        }
+
+        //test     //static?
+        reveal(x, y) {
+            if (cellsNumbers[x][y] === 0) {
+                cells[x][y].className = "known"
             }
-            cells[x][y].className = "known";
-            floodFill(x, y - 1);
-            floodFill(x + 1, y - 1);
-            floodFill(x + 1, y);
-            floodFill(x + 1, y + 1);
-            floodFill(x, y + 1);
-            floodFill(x - 1, y + 1);
-            floodFill(x - 1, y);
-            floodFill(x - 1, y - 1);
+            if (cellsNumbers[x][y] >= 1 && cellsNumbers[x][y] <= 8) {
+                cells[x][y].className = "known";
+                for (let i = 1; i < 9; i++) {
+                    if (cellsNumbers[x][y] === i) {
+                        cells[x][y].classList.add("number" + i);
+                        cells[x][y].innerText = cellsNumbers[x][y];
+                    }
+                }
+            }
+            if (cellsNumbers[x][y] === -1) {
+                for (let i = 0; i < cells[x].length; i++) {
+                    for (let j = 0; j < cells[y].length; j++) {
+                        cells[i][j].className = "known";
+                        if (cellsNumbers[i][j] === -1) {
+                            cells[i][j].style.backgroundImage = "url(images/saper.png)"
+                        }
+                        for (let k = 1; k < 9; k++) {
+                            if (cellsNumbers[i][j] === k) {
+                                cells[i][j].classList.add("number" + k);
+                                cells[i][j].innerText = cellsNumbers[i][j];
+                            }
+                        }
+                    }
+                }
+            }
+            if (this.checkNeighbours(x, y) === 0) {
+                floodFillCounter += 1;
+                this.floodFill(x, y)
+            }
         }
 
 
@@ -203,18 +269,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
-    const minesweeper = new Minesweeper(15, 15, 15);
+    const minesweeper = new Minesweeper(15, 15, 30);
     minesweeper.createBoard();
     minesweeper.createCellsNumbers();
 
+    saperPicture.addEventListener("mousedown", function () {
+        saperPicture.style.backgroundImage = "url(images/face_pressed.png)";
+    });
+    saperPicture.addEventListener("mouseup", () => {
+        saperPicture.style.backgroundImage = "url(images/face_unpressed.png)";
+        minesweeper.clearBoard();
+        minesweeper.createBoard();
+        minesweeper.createCellsNumbers();
+    });
+
     // minesweeper.helpFunctionToCheckEverything();
 
-    const gameMenu = $(".game");
-    const gameSubmenu = $(".gameSubmenu");
-    gameMenu.on("click", function () {
-        gameSubmenu.slideToggle();
+    const menuEasy = document.querySelector(".easy");
+    const menuMedium = document.querySelector(".medium");
+    const menuHard = document.querySelector(".hard");
+    const gameSubmenu = document.querySelector(".gameSubmenu");
+    menuEasy.addEventListener("click", () => {
+        this.minesweeper = null;
+        const minesweeper = new Minesweeper(9, 9, 10);
+        minesweeper.clearBoard();
+        minesweeper.createBoard();
+        minesweeper.createCellsNumbers();
+    });
+    menuMedium.addEventListener("click", () => {
+        this.minesweeper = null;
+        const minesweeper = new Minesweeper(15, 15, 30);
+        minesweeper.clearBoard();
+        minesweeper.createBoard();
+        minesweeper.createCellsNumbers();
+    });
+    menuHard.addEventListener("click", () => {
+        this.minesweeper = null;
+        const minesweeper = new Minesweeper(20, 20, 99);
+        minesweeper.clearBoard();
+        minesweeper.createBoard();
+        minesweeper.createCellsNumbers();
     })
-
 
 
 });
